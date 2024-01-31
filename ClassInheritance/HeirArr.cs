e5dee00c35babba16d4e1622886cdbb5c736acff
Anna Maximova <maksimova.anna@lit1533.ru>
2024-01-31 19:59:20 +0300

    3-2 version 2

--- ClassInheritance/HeirArr.cs
+++ ClassInheritance/HeirArr.cs
@@ -4,7 +4,7 @@ namespace ClassInheritance
 {
     public abstract class HeirArray
     {
-        public HeirArray() {}
+        public HeirArray() { }
         public abstract void OutArr();
         public abstract float Average();
     }
